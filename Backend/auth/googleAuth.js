060a3a4eeaa312881621b0843985df9e2a0c52d4
H. M. Adil Javaid <127618644+Adil-Javaid@users.noreply.github.com>
2024-09-08 11:48:58 +0500

    previous

--- Backend/auth/googleAuth.js
+++ Backend/auth/googleAuth.js
@@ -1,27 +1,27 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const userDB = require("../model/GoogleUserSchema");
-const session = require("express-session");
 
 const setupGoogleAuth = (app) => {
   const clientId = process.env.CLIENTID;
   const clientSecret = process.env.CLIENTSECRET;
   const secretSession = process.env.SECRET;
 
-  // Setup express-session middleware before passport
   app.use(
-    session({
+    require("express-session")({
       secret: secretSession,
       resave: false,
       saveUninitialized: false,
       cookie: {
-        httpOnly: false,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "none", // Required for cross-origin cookies
-        maxAge: 24 * 60 * 60 * 1000, // 24 hours
+        httpOnly: true, // Prevent JavaScript access
+        secure: true, // Cookies only sent over HTTPS
+        sameSite: "none", // Allows cross-site cookies
+        maxAge: 24 * 60 * 60 * 1000, // Session lasts for 24 hours
       },
     })
   );
+
+
 
   app.use(passport.initialize());
   app.use(passport.session());
@@ -42,7 +42,7 @@ const setupGoogleAuth = (app) => {
               googleId: profile.id,
               displayName: profile.displayName,
               email: profile.emails[0].value,
-              image: profile._json.picture, // Changed to profile._json.picture for profile image
+              image: profile.photo[0].value,
             });
             await user.save();
           }
@@ -54,9 +54,10 @@ const setupGoogleAuth = (app) => {
     )
   );
 
-  passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
+ passport.serializeUser((user, done) => {
+   done(null, user.id);
+ });
+
 
   passport.deserializeUser(async (id, done) => {
     try {
@@ -67,22 +68,21 @@ const setupGoogleAuth = (app) => {
     }
   });
 
+
   app.get(
     "/auth/google",
     passport.authenticate("google", { scope: ["profile", "email"] })
   );
 
   app.get(
-    "/auth/google/callback",
+    "https://book-app-virid-six.vercel.app/auth/google/callback",
     passport.authenticate("google", { failureRedirect: "/" }),
     (req, res) => {
-      console.log("Authenticated user: ", req.user);
       res.redirect("https://book-app-virid-six.vercel.app");
     }
   );
 
   app.get("/auth/user", (req, res) => {
-    console.log("Session details:", req.session);
     console.log("Is authenticated:", req.isAuthenticated());
     if (req.isAuthenticated()) {
       console.log("Authenticated user:", req.user);
