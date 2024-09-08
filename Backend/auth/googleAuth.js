077713136ba40c2b895a9bd5fdf08817e0f5da92
H. M. Adil Javaid <127618644+Adil-Javaid@users.noreply.github.com>
2024-09-08 14:32:40 +0500

    sample

--- Backend/auth/googleAuth.js
+++ Backend/auth/googleAuth.js
@@ -96,9 +96,7 @@ const setupGoogleAuth = (app) => {
     if (req.isAuthenticated() || forceAuthenticated) {
       try {
         // Find the user by ID (remove this if you just want to bypass it)
-        const user = await userDB.findById(
-          req.user?.id || "66d838c9727dfa9e0d5103c1"
-        );
+        const user = await userDB.findById(req.user?.id || "sampleUserId");
         if (user) {
           res.json({
             displayName: user.displayName,
